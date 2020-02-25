21d94aa188f0ef74b8a447ef0d52baa273421811
Bruno Luz <bruno.al.luz@gmail.com>
2020-02-25 13:39:36 -0300

    utc time

--- 01_new_environment.sh
+++ 01_new_environment.sh
@@ -1,2 +1,8 @@
 # files app - textarea on location instead of breadcrumbs
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+
+# files app - add "New Document" or right click
+touch ~/Templates/Empty\ Document
+
+# disable UTC and use local time (like windows)
+timedatectl set-local-rtc 1 --adjust-system-clock
