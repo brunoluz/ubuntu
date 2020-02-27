12e2bab21ce119ad85dd0c98bd49a524089d349a
Bruno Luz <bruno.al.luz@gmail.com>
2020-02-26 22:59:17 -0300

    usefull software and ubuntu extras repo

--- 01_new_environment.sh
+++ 01_new_environment.sh
@@ -6,3 +6,10 @@ touch ~/Templates/Empty\ Document
 
 # disable UTC and use local time (like windows)
 timedatectl set-local-rtc 1 --adjust-system-clock
+
+# install ubuntu-restricted-extras, which makes ubuntu able to
+# - Support for MP3 and unencrypted DVD playback
+# - Microsoft TrueType core fonts
+# - Flash plugin
+#  - codecs for common audio and video files
+sudo apt-get install ubuntu-restricted-extras -y
