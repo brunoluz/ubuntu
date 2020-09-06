453f2c42c51ed8ed722d8c23ecb34648a86a549c
brunoluz <bru_luz69@hotmail.com>
2020-09-05 21:06:05 -0300

    Update 01_new_environment.sh

--- 01_new_environment.sh
+++ 01_new_environment.sh
@@ -1,5 +1,5 @@
 # files app - textarea on location instead of breadcrumbs
-gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+# gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
 # files app - add "New Document" or right click
 touch ~/Templates/Empty\ Document
@@ -14,6 +14,7 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 #  - codecs for common audio and video files
 sudo apt-get install ubuntu-restricted-extras -y
 sudo apt-get remove ubuntu-restricted-extras -y
+sudo apt-get install chrome-gnome-shell -y
 
 # add open in terminal
 cp open-in-terminal.sh /home/bruno/.local/share/nautilus/scripts
