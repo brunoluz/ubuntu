e8ea785bc0ba0f78352e07ed1dcd091d6b305639
Bruno Luz <bruno.al.luz@gmail.com>
2020-09-26 19:18:33 -0300

    imwheel

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -30,3 +30,14 @@ sudo snap install --classic code
 # set desktop as solid color
 gsettings set org.gnome.desktop.background picture-uri ''
 gsettings set org.gnome.desktop.background primary-color 'rgb(66, 81, 100)'
+
+# imwhell 
+sudo apt install imwheel -y
+echo "\".*\"
+None,      Up,   Button4, 2
+None,      Down, Button5, 2
+Control_L, Up,   Control_L|Button4
+Control_L, Down, Control_L|Button5
+Shift_L,   Up,   Shift_L|Button4
+Shift_L,   Down, Shift_L|Button5" >> ~/.imwheelrc
+echo "imwheel" >> ~/.profile
